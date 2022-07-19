86c65aed9893f627aedaf047a6edbd6d81d31154
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-07-19 21:44:07 +0700

    done detail by id

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,45 +2,36 @@ const mongoose = require ("mongoose");
 const HotelSchema = new mongoose.Schema({
   name: {
     type: String,
-    //required: true,
   },
   type: {
     type: String,
-    //required: true,
   },
   city: {
     type: String,
-    // required: true,
   },
   address: {
     type: String,
-    // required: true,
   },
   distance: {
     type: String,
-    // required: true,
   },
   photos: {
     type: [String],
   },
   title: {
     type: String,
-    // required: true,
   },
   desc: {
     type: String,
-    // required: true,
   },
   rating: {
     type: Number,
     min: 0,
     max: 5,
   },
-  rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Rooms' }],
 
   cheapestPrice: {
     type: Number,
-    // required: true,
   },
   featured: {
     type: Boolean,
